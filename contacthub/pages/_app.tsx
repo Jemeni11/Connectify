c596d9921f6030b1f423f6e3d26bfac104a53fdd
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2023-09-08 19:48:30 +0100

    feat(_app.tsx): Moved fonts to _app.tsx and added some SEO stuff

--- contacthub/pages/_app.tsx
+++ contacthub/pages/_app.tsx
@@ -1,18 +1,42 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Navbar from "@/components/Navbar";
+import { Noto_Sans, Hind_Guntur, Rubik } from "next/font/google";
+
+const noto_sans_black = Noto_Sans({
+  weight: "900",
+  style: "normal",
+  subsets: ["latin"],
+});
+
+export const NotoSansFontClass = noto_sans_black.className;
+
+const hind_guntur_bold = Hind_Guntur({
+  weight: "700",
+  style: "normal",
+  subsets: ["latin"],
+});
+
+export const HindGuntarFontClass = hind_guntur_bold.className;
+
+const rubik = Rubik({
+  weight: "500",
+  style: "normal",
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <meta name="application-name" content="ContactHub" />
+        <meta name="application-name" content="ConnexusLink" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="ContactHub" />
+        <meta name="apple-mobile-web-app-title" content="ConnexusLink" />
         <meta
           name="description"
-          content="ContactHub is a simple yet powerful contact management application that allows users to store and organize all of their contacts in one place. "
+          content="ConnexusLink is a simple yet powerful contact management application that allows users to store and organize all of their contacts in one place."
         />
         <meta name="format-detection" content="telephone=yes" />
         <meta name="mobile-web-app-capable" content="yes" />
@@ -24,60 +48,28 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="theme-color" content="#000000" />
         {/* TODO: Add an apple touch icon */}
         <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
-        <link
-          rel="apple-touch-icon"
-          sizes="152x152"
-          href="/icons/touch-icon-ipad.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/icons/touch-icon-iphone-retina.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="167x167"
-          href="/icons/touch-icon-ipad-retina.png"
-        />
+        <link rel="apple-touch-icon" sizes="152x152" href="/icons/touch-icon-ipad.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/touch-icon-iphone-retina.png" />
+        <link rel="apple-touch-icon" sizes="167x167" href="/icons/touch-icon-ipad-retina.png" />
 
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/icons/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/icons/favicon-16x16.png"
-        />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
         <link rel="manifest" href="/manifest.json" />
-        <link
-          rel="mask-icon"
-          href="/icons/safari-pinned-tab.svg"
-          color="#5bbad5"
-        />
+        <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
         <link rel="shortcut icon" href="/icons/favicon.ico" />
         {/* TODO: Change this */}
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:url" content="https://yourdomain.com" />
-        <meta name="twitter:title" content="PWA App" />
+        <meta name="twitter:title" content="ConnexusLink" />
         <meta name="twitter:description" content="Best PWA App in the world" />
-        <meta
-          name="twitter:image"
-          content="https://yourdomain.com/icons/android-chrome-192x192.png"
-        />
-        <meta name="twitter:creator" content="@DavidWShadow" />
+        <meta name="twitter:image" content="https://yourdomain.com/icons/android-chrome-192x192.png" />
+        <meta name="twitter:creator" content="@Jemeni11" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="PWA App" />
         <meta property="og:description" content="Best PWA App in the world" />
         <meta property="og:site_name" content="PWA App" />
         <meta property="og:url" content="https://yourdomain.com" />
-        <meta
-          property="og:image"
-          content="https://yourdomain.com/icons/apple-touch-icon.png"
-        />
+        <meta property="og:image" content="https://yourdomain.com/icons/apple-touch-icon.png" />
 
         {/* apple splash screen images */}
 
@@ -90,7 +82,10 @@ export default function App({ Component, pageProps }: AppProps) {
           <link rel='apple-touch-startup-image' href='/images/apple_splash_640.png' sizes='640x1136' />
         */}
       </Head>
-      <Component {...pageProps} />
+      <div className={rubik.className}>
+        <Navbar />
+        <Component {...pageProps} />
+      </div>
     </>
   );
 }
