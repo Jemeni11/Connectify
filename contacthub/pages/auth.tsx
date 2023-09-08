86b60ea303d8fdeb5a80beab6d2a14b0b5fcf25d
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2023-09-08 19:52:39 +0100

    feat: Created new files

---
+++ contacthub/pages/auth.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Router, { useRouter } from "next/router";
+
+function Auth() {
+  const router = useRouter();
+  return <div>Auth</div>;
+}
+
+export default Auth;
