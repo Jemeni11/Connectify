dc1294a8c34f38d097952b41e7cd0faa9e7da74e
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2023-09-08 19:51:14 +0100

    refactor(_offline.tsx): Changed names

--- contacthub/pages/_offline.tsx
+++ contacthub/pages/_offline.tsx
@@ -3,9 +3,9 @@ import Head from "next/head";
 const Fallback = () => (
   <>
     <Head>
-      <title>ContactHub | You're offline!</title>
+      <title>ConnexusLink | You're offline!</title>
     </Head>
-    <h1>This is ContactHub's offline fallback page</h1>
+    <h1>This is ConnexusLink's offline fallback page</h1>
     <h2>
       You are currently not connected to the internet. Please connect and try
       again.
