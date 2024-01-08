d68c3d53b2070320717ee2823e45460f7dff4945
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2024-01-08 13:53:24 +0100

    feat(Announcement.tsx): Created a new component

---
+++ components/Announcement.tsx
@@ -0,0 +1,12 @@
+export default function Announcement() {
+  return (
+    <div className="bg-duskyRed px-4 py-3 text-white">
+      <p className="text-center text-sm font-medium">
+        Love Connectify?{" "}
+        <a href="https://github.com/Jemeni11/Connectify" className="inline-block underline">
+          Give it a star on GitHub!
+        </a>
+      </p>
+    </div>
+  );
+}
