938875ca4992dff65cbb9c6e1eafc6aad141b1d2
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2024-01-10 14:50:39 +0100

    build(next.config.js): Next is giving errors so I commented out the previous code

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const withPWA = require("@ducanh2912/next-pwa").default({
 });
 
 const nextConfig = withPWA({
-  cacheOnFrontEndNav: true,
+  // cacheOnFrontEndNav: true,
   images: {
     remotePatterns: [
       {
