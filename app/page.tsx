6d3955ca588f603002e57990c02589cbbbf59842
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2024-01-10 10:30:01 +0100

    moved OtherFeatures to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ type featuresGridItemType = { title: string; icon: StaticImageData; description:
 
 function FeaturesGridItem({ title, icon, description }: featuresGridItemType) {
   return (
-    <div className="flex items-start gap-4 FeaturesGridItem">
+    <div className="FeaturesGridItem flex items-start gap-4">
       <span className="shrink-0 p-4">
         <Image src={icon} alt={title} className="h-5 w-5 text-duskyRed md:h-8 md:w-8" />
       </span>
@@ -69,6 +69,47 @@ const featuresPartOne: featuresGridItemType[] = [
     title: "Contact Communication",
     icon: Logo,
     description: "Stay connected with a single click. Call, email, or message your contacts with the app.",
+  },
+];
+
+const featuresPartTwo: { title: string; content: string }[] = [
+  {
+    title: "Contact Sharing",
+    content:
+      "Share contact information with others effortlessly. Whether it's sending a colleague's contact details to a client or sharing a friend's info with a group, ConnexusLink makes it simple.",
+  },
+  {
+    title: "Contact Notes",
+    content:
+      "Keep track of important details. Add personal notes or reminders for each contact. Never forget a birthday or an important detail again.",
+  },
+  {
+    title: "Contact Birthdays/Reminders",
+    content:
+      "Never miss a special day. ConnexusLink helps you remember birthdays, anniversaries, and other important dates. Set reminders and celebrate with your loved ones.",
+  },
+  {
+    title: "Contact Deletion",
+    content:
+      "Declutter your contacts. Remove outdated or irrelevant contacts easily. ConnexusLink ensures your contact list stays clean and organized.",
+  },
+  {
+    title: "Responsive Design",
+    content:
+      "Access your contacts from anywhere, on any device. ConnexusLink is fully responsive, ensuring a seamless experience whether you're on your phone, tablet, or desktop.",
+  },
+  {
+    title: "User Settings",
+    content: "Tailor ConnexusLink to your preferences. Customize themes, language, and privacy settings to make the app truly your own.",
+  },
+  {
+    title: "User Profile",
+    content: "Personalize your profile. Update your information and profile picture to keep your ConnexusLink experience unique.",
+  },
+  {
+    title: "Error Handling and Validation",
+    content:
+      "Enjoy a hassle-free experience. ConnexusLink is designed with meticulous error handling and validation to ensure smooth interactions and data accuracy.",
   },
 ];
 
